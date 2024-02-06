46aae3908f7bc0c36b529e1db73460f0170511a7
restlessronin <88921269+restlessronin@users.noreply.github.com>
2024-02-06 18:12:29 +0530

    name cleanup

--- src/__test__/codefixture.ts
+++ src/__test__/codefixture.ts
@@ -24,4 +24,39 @@ print(my_function(3, 4))
 `,
 };
 
-export const allSources = [test_file_with_identifiers, test_file_import, test_file_pass];
+export const test_typescript_code = `import { Tag } from '../tagger';
+
+export class RankedTags {
+  constructor(
+    readonly definitions: Map<string, Set<Tag>>,
+    readonly rankedFiles: [string, number][],
+    readonly rankedDefinitions: [string, number][]
+  ) {}
+
+  without(chatRelPaths: string[]) {
+    const filteredFiles = this.rankedFiles.filter(
+      ([relPath, _]) => !chatRelPaths.includes(relPath)
+    );
+    const filteredDefinitions = this.rankedDefinitions.filter(([key, _]) => {
+      const [relPath, _ident] = key.split(',');
+      return !chatRelPaths.includes(relPath);
+    });
+    return new RankedTags(this.definitions, filteredFiles, filteredDefinitions);
+  }
+
+  toTags() {
+    return this.rankedDefinitions.reduce((acc: Tag[], [key, _rank]: [string, number]) => {
+      return [...acc, ...Array.from(this.definitions.get(key) as Set<Tag>)];
+    }, []);
+  }
+
+  toRankedFiles(files: string[]) {
+    const missingFiles = files.filter(
+      file => !this.rankedFiles.some(([relPath, _]) => relPath === file)
+    );
+    return [...this.rankedFiles.map(([relPath, _rank]) => relPath), ...missingFiles];
+  }
+}
+`;
+
+export const pythonSources = [test_file_with_identifiers, test_file_import, test_file_pass];
--- src/__test__/ranker.test.ts
+++ src/__test__/ranker.test.ts
@@ -1,17 +1,15 @@
-import * as path from 'path';
-import { TagRanker } from '../ranker/TagRanker';
-
-import { allSources } from './codefixture';
 import { CodeTagExtractor } from '../tagger/CodeTagExtractor';
 import { NodeContentPath } from '../tagger/ContentPath.node';
 import { DefRefs } from '../tagger/DefRefs';
+import { TagRanker } from '../ranker/TagRanker';
+import { pythonSources } from './codefixture';
 
 describe('TagRanker', () => {
   let tagRanker: TagRanker;
 
   beforeEach(async () => {
     const extractor = new CodeTagExtractor('', new NodeContentPath());
-    const defRefs = await DefRefs.create(extractor, allSources);
+    const defRefs = await DefRefs.create(extractor, pythonSources);
     const tags = defRefs!.createTags();
     tagRanker = new TagRanker(tags!);
   });
